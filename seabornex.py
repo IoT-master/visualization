432bbca0807a832db5ba683718e38178125d8dc1
Senh Mo Chuang <senhmo@gmail.com>
2019-09-30 21:11:21 -0500

    Added Seaborn

--- seabornex.py
+++ seabornex.py
@@ -1,29 +1,21 @@
 import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-
-plt.style.use('seaborn')
+import seaborn as sns
+# plt.style.use('seaborn')
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111, projection='3d')
-
-# x = [1,2,3,4,5,6,7,8,9,10]
-# y = [1,2,3,4,5,6,7,8,9,10]
-# z = [1,2,3,4,5,6,7,8,9,10]
-
-# ax1.plot(x,y,z)
-
-# ax1.set_xlabel('x')
-# ax1.set_ylabel('y')
-# ax1.set_zlabel('z')
-# plt.show()
 
 raw_housing_data = pd.read_csv('Boston_Housing_Red_4160_Regression.csv')
 rm = raw_housing_data.RM
 dis = raw_housing_data.DIS
 price = raw_housing_data.Price
-ax1.scatter(rm, dis, price)
-ax1.set_xlabel('rm')
-ax1.set_ylabel('dis')
-ax1.set_zlabel('price')
+# ax1.scatter(rm, dis, price)
+# ax1.set_xlabel('rm')
+# ax1.set_ylabel('dis')
+# ax1.set_zlabel('price')
+# plt.show()
+
+g = sns.pairplot(raw_housing_data)
 plt.show()
